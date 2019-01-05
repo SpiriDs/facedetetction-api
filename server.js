d652741890f138b80af0156caf17316c309dac99
SpiriDs <spirids@live.de>
2019-01-05 23:52:06 +0700

    Server ueberarbeitet  Controller eingfuehrt

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const knex = require("knex");
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -23,91 +28,26 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
 app.get("/", (req, res) => {
   res.send(database.users);
 });
 
 /* Sign In Post*/
-app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db.select("*").from("users")
-          .where("email", "=", req.body.email)
-          .then(user => {
-            res.json(user[0]);
-          })
-          .catch(err => res.status(400).json("unable to get user"))
-      } else {
-        res.status(400).json("wrong credentials")
-      }
-    })
-    .catch(err => res.status(400).json("wrong credentials"));
-});
+app.post("/signin", signin.handleSignin(db, bcrypt)); //Andere Art des Aufrufs, es wird die funktion mit db, bcrypt aufgerufen und in signin.js ruft dies funktion die funktion mit (req,res aufgerufen)
 
 /* Register Post */
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("unable to register"));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 /* Profile mit user id */
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({
-      id: id
-    })
-    .then(user => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("User not found");
-      }
-    })
-    .catch(err => res.status(400).json("Error finding User"));
+  profile.handleGetProfile(req, res, db);
 });
 
 /* /image --> PUT = user :update */
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then(entries => {
-      res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json("Unable to get entries"));
+  image.handleImage(req, res, db);
 });
 
 app.listen(3000, () => {
