9fc2390dd952451f10b85a500249933d37f4d530
SpiriDs <spirids@live.de>
2020-09-09 22:38:35 +0700

    Deploy DB on Heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-globular-18448",
     user: "spirids",
     password: "P12345",
     database: "smartbrain",
